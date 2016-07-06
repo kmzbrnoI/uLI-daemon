2a3a9e77712ec7dae02885f55ac107545abd2754
Jan Horacek <jan.horacek@seznam.cz>
2016-07-06 11:14:10 +0200

    Povolena pouze jedna instance uLI-daemon.

--- src/uLIdaemon.dpr
+++ src/uLIdaemon.dpr
@@ -4,12 +4,25 @@ program uLIdaemon;
 uses
   Forms,
   Windows,
+  SysUtils,
   fMain in 'fMain.pas' {F_Main};
 
 {$R *.res}
 
+var
+  Mutex: Cardinal;
+
 begin
   SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS);
+  DecimalSeparator := '.';
+
+  // povolena pouze jedna instance uLI-daemon
+  Mutex := CreateMutex(nil, True, 'uLI-daemon');
+  if ((Mutex = 0) or (GetLastError = ERROR_ALREADY_EXISTS)) then
+   begin
+    Application.MessageBox('uLI-daemon již spuštìn, povolena pouze jedna instance.', 'Již spuštìn', MB_ICONWARNING OR MB_OK);
+    halt(0);
+   end;
 
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
